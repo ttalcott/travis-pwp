13c415eccd774cf1374eaab7c7571af7ab741bae
Travis Talcott <ttalcott@aquilagroup.com>
2016-07-20 14:54:41 -0600

    added a persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -30,5 +30,21 @@
 				My goals for my personal website are to attract future clients and to showcase my abilities as a web developer.
 			</p>
 		</div>
+		<h3>Persona:</h3>
+		<ul>
+			<li>Name: John Smith</li>
+			<li>Age: 53</li>
+			<li>Occupation: John is a long time CEO of a company that sells technology.</li>
+			<li>Technologies used: John uses a 15in Macbook Pro with Retina Display but also likes surfing the web on his iPhone 6 plus while sitting in a restaurant at 7pm waiting for his food.</li>
+			<li>Current frustrations: John's company has a large list of clients who would like to buy their products online but their current website is outdated and doesn't support online shopping. Also, their current site is not responsive whatsoever and has terrible UX. The colors are ugly, the html itself uses a bunch of deprecated tags, and no one wants to view their site because, well, it isn't pretty.</li>
+			<li>Needs: John is looking to find a development company who can do a complete overhaul of their site and create a database for them so they can sell their products online. He would like to support a small business (since that's how he started out) but needs to find someone who can give the company exactly what they want.</li>
+			<li>Goals with viewing my site: John wants to see a development company's capability by viewing their website. He is also hoping to find information about the company as well as come in contact with them. It would be an added bonus if he could request a quote through the website.</li>
+		</ul>
+		<h3>Use Case:</h3>
+		<ul>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
 	</body>
 </html>
