27357796aae079c48c983bd38e6e011a906af36d
Travis Talcott <ttalcott@aquilagroup.com>
2016-07-20 14:22:33 -0600

    added persona and audience paragraphs

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,6 +7,22 @@
 		</title>
 	</head>
 	<body>
-		<p>Test</p>
+		<header>
+			<h1>
+				Lyra Development Solutions
+			</h1>
+		</header>
+		<h3>Purpose:</h3>
+		<div class="paragraphs">
+			<p>
+				The purpose of my personal website project is to develop the website for my start up company, Lyra Development Solutions.
+			</p>
+		</div>
+		<h3>Audience:</h3>
+		<div class="paragraphs">
+			<p>
+				The people who will be viewing y site will be future clients, customers, and hopefully fellow developers. I hope my company will grow to a size where future employees will also look at the website to see who they will be working with.
+			</p>
+		</div>
 	</body>
 </html>
