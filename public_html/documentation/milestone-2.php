f5539c3950c179269e8d8c566f8d7ebc1040e857
Travis Talcott <ttalcott@aquilagroup.com>
2016-08-08 16:48:38 -0600

    added a content strategy section

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Milestone 2a</title>
+	</head>
+	<body>
+		<header>
+			<h1>Lyra Development Milestone 2 Alpha</h1>
+		</header>
+		<div class="map">
+			<h2>Site Map:</h2>
+			<img src="images/lyraSiteMap.png" alt="Lyra Site Map" />
+			<p>
+				I want to keep my site as simple as possible so the user has a very easy time navigating the pages and finding the information they need.
+			</p>
+		</div>
+		<div class="content">
+			<h2>Content Strategy:</h2>
+			<div class="strategy">
+				<h5>Home page:</h5>
+					<p>
+						The content for my home page is going to be simple. I want to do a very graphic full page background image with some catchy text about how one should choose Lyra Development Solutions for their work. The top nav will consist of a logo on the far left, the company name, and 3 buttons that choose either "About", "Our Work", or "Contact". The header itself will be clear so that you can really see the full background image. The footer will contain copyright and contact information as well as links to our social media profiles.
+					</p>
+				<h5>About:</h5>
+					<p>
+						The about page will house information of Lyra Development Solutions. The first section is going to be a paragraph bout Lyra Development Solutions and it will say what we do and why we do it. The next section will be an image (or images) of our team members with a caption below detailing their name. The third section will contain bios of everyone who works there. The footer will stay the same on every page.
+					</p>
+				<h5>Our Work:</h5>
+					<p>
+						The Our Work section will basically be a place where we house examples and details about our current customers and all the projects we have worked on. The first part of this page will be a list of all current customers (small at first but hopefully large later on!) and also a list of all of our projects. Following will be an image or two (later on hopefully an image gallery) of the projects we have done that when hovered over will reveal the name of the site and a small description. The last section will contain 2 columns; the first of which will describe the services we offer and the second will detail some package deals they can choose from. The footer will stay the same on every page.
+					</p>
+				<h5>Contact:</h5>
+					<p>
+						The contact section will contain an almost full page contact form the user can submit and send Lyra Development Solutions an email regarding either wanting more information or requesting a quote for either a service or package. Below the contact section will be a small text area where we say something catchy about them working with us. The footer will stay the same on every page.
+					</p>
+			</div>
+		</div>
+		<div class="frames">
+			
+		</div>
+	</body>
+</html>
