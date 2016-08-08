111312e535312ff9732d7690f9c3edb3f878b71c
Travis Talcott <ttalcott@aquilagroup.com>
2016-08-08 13:08:43 -0600

    added a site map and header

---
+++ public_html/documentation/milestone-2a.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Milestone 2a</title>
+	</head>
+	<body>
+		<header>
+			<h1>Lyra Development Milestone 2 Alpha</h1>
+		</header>
+		<div class="map">
+			<h2>Site Map:</h2>
+			<img src="images/lyraSiteMap.png" alt="Lyra Site Map" />
+			<p>
+				I want to keep my site as simple as possible so the user has a very easy time navigating the pages and finding the information they need.
+			</p>
+		</div>
+	</body>
+</html>
