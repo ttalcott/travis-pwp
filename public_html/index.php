862a62315bcea0c2ce49274db515563eedc8952f
Travis Talcott <ttalcott@aquilagroup.com>
2016-08-29 08:27:47 -0600

    added images?

---
+++ public_html/index.php
@@ -0,0 +1,17 @@
+<?php
+/*grab current directory*/
+$CURRENT_DIR = __DIR__;
+
+// set page title
+$PAGE_TITLE = "LDS";
+
+//load head-utils.php
+require_once("php/partials/head-utils.php");
+?>
+<body class="sfooter home-page">
+	<div class="sfooter-content">
+		<div class="jumbotron">
+			
+		</div>
+	</div>
+</body>
--- public_html/php/partials/head-utils.php
+++ public_html/php/partials/head-utils.php
@@ -4,9 +4,7 @@
  * @see https://raw.githubusercontent.com/kingscreations/farm-to-you/master/php/lib/_header.php FarmToYou Header
  **/
 
-// include the appropriate number of dirname() functions
-// on line 8 to correctly resolve your directory's path
-require_once(dirname(dirname(__DIR__)) . "/root-path.php");
+require_once(dirname((__DIR__), 2) . "/root-path.php");
 $CURRENT_DEPTH = substr_count($CURRENT_DIR, "/");
 $ROOT_DEPTH = substr_count($ROOT_PATH, "/");
 $DEPTH_DIFFERENCE = $CURRENT_DEPTH - $ROOT_DEPTH;
