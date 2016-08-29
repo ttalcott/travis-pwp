67b57e8796a6cb645dece0e3f0fcb29ab9423fd3
Travis Talcott <ttalcott@aquilagroup.com>
2016-08-29 10:09:28 -0600

    added new logo

--- public_html/php/partials/header.php
+++ public_html/php/partials/header.php
@@ -3,7 +3,7 @@
 		<div class="container-fluid">
 			<div class="navbar-header">
 				<a class="navbar-brand" href="/~ttalcott/travis-pwp/public_html/index.php">
-					<img src="../../images/lyra-logo.svg" alt="Lyra Logo" />
+					<img src="images/lyra-logo.svg" alt="Lyra Logo" />
 				</a>
 			</div>
 		</div>
