5f5d3aa01c4ef0d494b029fc270c8abd7ab72e61
Travis Talcott <ttalcott@aquilagroup.com>
2016-08-30 16:27:37 -0600

    changed some styles

---
+++ public_html/about/index.php
@@ -0,0 +1,16 @@
+<?php
+
+//grab current Directory
+$CURRENT_DIR = __DIR__;
+
+//set page name
+$PAGE_TITLE = "About LDS";
+
+//load HTML head tag
+require_once(dirname(__DIR__) . "/php/partials/head-utils.php");
+ ?>
+<body class="sfooter all-else">
+	<div class="sfooter-content">
+		<?php require_once(dirname(__DIR__) . "/php/partials/header.php"); ?>
+	</div>
+</body>
