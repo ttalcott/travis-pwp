2c467973378346b901d973b8575ef311e41cbef8
Travis Talcott <ttalcott@aquilagroup.com>
2016-08-30 11:57:52 -0600

    added image

--- public_html/index.php
+++ public_html/index.php
@@ -17,7 +17,7 @@ require_once("php/partials/head-utils.php");
 			<div class="container">
 				<div class="row">
 					<div class="col-md-6 col-md-offset-3">
-						<div class="catchy" id="">
+						<div class="catchy animated fadeInUp">
 							<img src="images/lyra-logo.svg" alt="Lyra Logo" />
 						</div>
 					</div>
