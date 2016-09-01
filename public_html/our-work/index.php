19b6a8dd5e0bbe487d182ac3da2ff15e5f59be20
Travis Talcott <ttalcott@aquilagroup.com>
2016-09-01 00:34:49 -0600

    added headers to all pages

---
+++ public_html/our-work/index.php
@@ -0,0 +1,17 @@
+<?php
+
+//grab current Directory
+$CURRENT_DIR = __DIR__;
+
+//set page name
+$PAGE_TITLE = "Our Work";
+
+//load HTML head tag
+require_once(dirname(__DIR__) . "/php/partials/head-utils.php");
+?>
+
+<body class="sfooter others">
+	<div class="sfooter-content background2">
+		<?php require_once($PREFIX . "php/partials/header.php"); ?>
+	</div>
+</body>
