14f94edb45d6d207382c42b624b3e29f635855ed
Travis Talcott <ttalcott@aquilagroup.com>
2016-09-09 00:51:02 -0600

    last of the day made footer stuff centered

--- public_html/php/partials/footer.php
+++ public_html/php/partials/footer.php
@@ -1,11 +1,11 @@
 <footer class="footer">
 	<div class="container-fluid">
-		<div class="col-md-6 footer-content">
+		<div class="col-md-6 footer-content-1">
 			<span class="info">
 				Copyright &copy; Travis Talcott
 			</span>
 		</div>
-		<div class="col-md-6 footer-content">
+		<div class="col-md-6 footer-content-2">
 			<span>
 				<span class="gold">Email:</span> <span class="info">info@lyradevelopment.com</span>
 			<span>
