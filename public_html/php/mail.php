2427e55206076b09ee6f7164bdf168a6906cdbe1
Travis Talcott <ttalcott@aquilagroup.com>
2016-09-26 12:18:13 -0600

    changes to email stuff

--- public_html/php/mail.php
+++ public_html/php/mail.php
@@ -74,8 +74,8 @@ try {
   }
 
   // report a successful send
-  echo "<div class=\"alert alert-success\" role=\"alert\">Email successfully sent.</div>";
+  echo "<div class=\"alert alert-success\" role=\"alert\">Email sent successfully.</div>";
 
 } catch(Exception $exception) {
-  echo "<div class=\"alert alert-danger\" role=\"alert\"><strong>Oh snap!</strong> Unable to send email: " . $exception->getMessage() . "</div>";
+  echo "<div class=\"alert alert-danger\" role=\"alert\">Unable to send email: " . $exception->getMessage() . "</div>";
 }
